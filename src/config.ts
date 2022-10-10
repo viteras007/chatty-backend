bb46d18f34b2db8068ed6b3a90028046b93b155a
viteras007 <viteras10@gmail.com>
2022-10-10 16:13:39 -0300

    set up project

---
+++ src/config.ts
@@ -0,0 +1,44 @@
+import dotenv from 'dotenv';
+import bunyan from 'bunyan';
+
+dotenv.config({});
+
+class Config {
+  public DATABASE_URL: string | undefined;
+  public JWT_TOKEN: string | undefined;
+  public NODE_ENV: string | undefined;
+  public SECRET_KEY_ONE: string | undefined;
+  public SECRET_KEY_TWO: string | undefined;
+  public CLIENT_URL: string | undefined;
+  public REDIS_HOST: string | undefined;
+
+  private readonly DEFAULT_DATABASE_URL =
+    'mongodb+srv://viteras007:victore1r2t3@cluster0.nrlxa1w.mongodb.net/chattyapp-backend';
+
+  constructor() {
+    this.DATABASE_URL = process.env.DATABASE_URL || this.DEFAULT_DATABASE_URL;
+    this.JWT_TOKEN = process.env.JWT_TOKEN || '1234';
+    this.NODE_ENV = process.env.NODE_ENV || '';
+    this.SECRET_KEY_ONE = process.env.SECRET_KEY_ONE || '';
+    this.SECRET_KEY_TWO = process.env.SECRET_KEY_TWO || '';
+    this.CLIENT_URL = process.env.CLIENT_URL || '';
+    this.REDIS_HOST = process.env.REDIS_HOST || '';
+  }
+
+  public createLogger(name: string): bunyan {
+    return bunyan.createLogger({
+      name,
+      level: 'debug',
+    });
+  }
+
+  public validateConfig(): void {
+    for (const [key, value] of Object.entries(this)) {
+      if (value === undefined) {
+        throw new Error(`configuration ${key} is undefined`);
+      }
+    }
+  }
+}
+
+export const config: Config = new Config();
---
+++ src/setupDatabase.ts
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+import { config } from './config';
+import Logger from 'bunyan';
+
+const log: Logger = config.createLogger('setupDatabase');
+
+export default () => {
+  const connect = () => {
+    mongoose
+      .connect(`${config.DATABASE_URL}`)
+      .then(() => {
+        log.info('Connected to MongoDB');
+      })
+      .catch((error) => {
+        log.error('Failed to connect to MongoDB', error);
+        return process.exit(1);
+      });
+  };
+  connect();
+
+  mongoose.connection.on('disconnect', connect);
+};
