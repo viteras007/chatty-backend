92a39155b102f3183eb81d89d58936e069949838
viteras007 <viteras10@gmail.com>
2022-10-10 16:37:07 -0300

    fixing and add pathing [ttypescript]

--- src/setupServer.ts
+++ src/setupServer.ts
@@ -1,8 +1,4 @@
 import {
-  CustomError,
-  IErrorResponse,
-} from './shared/globals/helpers/error-handler';
-import {
   Application,
   json,
   urlencoded,
@@ -22,8 +18,9 @@ import { createClient } from 'redis';
 import { createAdapter } from '@socket.io/redis-adapter';
 import Logger from 'bunyan';
 import 'express-async-errors';
-import { config } from './config';
-import applicationRoutes from './routes';
+import { config } from '@root/config';
+import applicationRoutes from '@root/routes';
+import { CustomError, IErrorResponse } from '@global/helpers/error-handler';
 
 const SERVER_PORT = 5000;
 const log: Logger = config.createLogger('server');
@@ -129,5 +126,7 @@ export class ChattyServer {
     });
   }
 
-  private socketIOConnections(io: Server): void {}
+  private socketIOConnections(io: Server): void {
+    log.info('socketIOConnections');
+  }
 }
